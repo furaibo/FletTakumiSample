5512c24914e656d2ce56838991feb9ee21c93e69
furaibo <furaiboxyz@gmail.com>
2025-05-14 19:32:31 +0900

    rename folder/files and add samples

---
+++ chapter2_samples/sample1-06_container.py
@@ -0,0 +1,41 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.title = "Containers - clickable and not"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    page.add(
+        ft.Row(
+            [
+                ft.Container(
+                    content=ft.Text("Non clickable"),
+                    margin=10,
+                    padding=10,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.Colors.AMBER,
+                    width=150,
+                    height=150,
+                    border_radius=10,
+                ),
+                ft.Container(
+                    content=ft.Text("Clickable without Ink"),
+                    margin=10,
+                    padding=10,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.Colors.GREEN_200,
+                    width=150,
+                    height=150,
+                    border_radius=10,
+                    on_click=lambda e: print("Clickable without Ink clicked!"),
+                ),
+                # (...中略...)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+    )
+
+
+ft.app(main)
